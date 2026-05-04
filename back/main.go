b5abe372c3de55bbc52721aa050bc5064462aa27
Juampi095 <azarjpm15@gmail.com>
2026-05-04 15:25:25 -0300

    fix:Se permite dev y docker para que sean compatibles con CORS

--- back/main.go
+++ back/main.go
@@ -7,10 +7,18 @@ import (
 	"tasks/back/handler"
 )
 
+var allowedOrigins = map[string]bool{
+	"http://localhost:5173": true,
+	"http://localhost":      true,
+}
+
 func corsMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Access-Control-Allow-Origin", "http://localhost:5173")
-		w.Header().Set("Access-Control-Allow-Methods", "GET, POST , PUT, DELETE, OPTIONS")
+		origin := r.Header.Get("Origin")
+		if allowedOrigins[origin] {
+			w.Header().Set("Access-Control-Allow-Origin", origin)
+		}
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
 		w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
 
 		if r.Method == http.MethodOptions {
